ead3b8538cbc7abbcc6873a1fb09380880be99de
Ruben Villalvazo <vim.ruben@hotmail.com>
2021-09-15 01:19:07 -0300

    Requested weather speech answer

--- clima.py
+++ clima.py
@@ -1,17 +1,22 @@
 import requests, json
+from googletrans import Translator
+
+translator = Translator()
 
 api_key = "f1e4695138a9aa78063b3515d1b84957"
-city_name = input("Enter the city name: ")
+city_name = "Curitiba"#input("Enter the city name: ")
 requests_base_url = (f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}")
 response = requests.get(requests_base_url)
 x = response.json()
 if x["cod"] != "404":
     y = x["main"]
-    current_temperature = y["temp"]
+    current_temperature_kelvin = y["temp"]
+    current_temperature = "{:.2f}".format(current_temperature_kelvin - 273.15) 
     current_pressure = y["pressure"]
     current_humidity = y["humidity"]
     z = x["weather"]
-    weather_description = z[0]["description"]
-    print(f"Temperature (in kelvin): {str(current_temperature)} \n Atmospheric presure (in hPa): {str(current_pressure)} \n Humidity percentage: {str(current_humidity)} \n Description: {str(weather_description)}")
+    weather_description_eng = z[0]["description"]
+    weather_description = translator.translate(weather_description_eng, dest='es', src='en')
+    print(f"La temperatura es de {str(current_temperature)}°C, presión Atmosferica de {str(current_pressure)} hPa, {str(current_humidity)}% de humedad, con {str(weather_description.text)}")
 else:  
     print("City not found")
